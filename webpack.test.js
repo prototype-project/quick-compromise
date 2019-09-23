00b14ac17dbdc0193363cd110660b6a5fa80c0f1
Daniel Faderski <daniel.faderski@allegro.pl>
2019-09-23 23:31:04 +0200

    initial

---
+++ webpack.test.js
@@ -0,0 +1,18 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    target: "node",
+    externals: [nodeExternals()],
+    mode: "development"
+};
